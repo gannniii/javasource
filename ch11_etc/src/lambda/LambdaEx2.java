cd5b846cf1fd31e250bc01d6093e0831bae267dd
gannniii <kke08kr@gmail.com>
2025-03-21 11:32:05 +0900

    stream

--- ch11_etc/src/lambda/LambdaEx2.java
+++ ch11_etc/src/lambda/LambdaEx2.java
@@ -1,19 +1,23 @@
 package lambda;
 
 import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
 import java.util.function.Consumer;
 import java.util.function.DoubleConsumer;
+import java.util.function.Function;
 import java.util.function.IntConsumer;
 import java.util.function.IntSupplier;
 import java.util.function.LongConsumer;
 import java.util.function.ObjDoubleConsumer;
+import java.util.function.Predicate;
 import java.util.function.Supplier;
 
 public class LambdaEx2 {
     public static void main(String[] args) {
         // void accept(T t);
         // 숫자를 전달인자로 받아 출력하기
-        Consumer<Integer> consumer = (i) -> System.out.println(i);
+        // (i) -> System.out.println(i);
+        Consumer<Integer> consumer = System.out::println;
         consumer.accept(50);
 
         // 문자를 전달인자로 받아 출력
@@ -38,6 +42,34 @@ public class LambdaEx2 {
 
         IntSupplier intSupplier = () -> (int) (Math.random() * 6) + 1;
         System.out.println(intSupplier.getAsInt());
+
+        // 전달인자로 받은 문자열의 길이가 0인지 테스트한 후 0인 경우 빈문자열입니다 출력
+        Predicate<String> predicate = (str) -> str.length() == 0;
+        if (predicate.test(""))
+            System.out.println("빈 문자열 입니다");
+
+        // 전달인자로 받은 숫자가 짝수인지 테스트한 후 짝수라면 짝수. 아닌경우 홀수
+        Predicate<Integer> predicate2 = (i) -> i % 2 == 0;
+        if (predicate2.test(5)) {
+            System.out.println("짝수");
+        } else {
+            System.out.println("홀수");
+        }
+
+        // T : type, R : return type
+        // 숫자를 문자로 반환
+        Function<Integer, String> f = (i) -> String.valueOf(i);
+        System.out.println(f.apply(35));
+
+        // 사칙연산
+        BiFunction<Integer, Integer, Integer> f2 = (x, y) -> x + y;
+        System.out.println(f2.apply(5, 4));
+
+        f2 = (x, y) -> x * y;
+        System.out.println(f2.apply(5, 4));
+
+        f2 = (x, y) -> x - y;
+        System.out.println(f2.apply(5, 4));
 
     }
 }
