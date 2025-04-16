071e43e053983d286c7a35e151a7dc6a6e1ec047
gannniii <kke08kr@gmail.com>
2025-04-16 18:01:45 +0900

    board

---
+++ ch12_jdbc/src/basic/BoardDTO.java
@@ -0,0 +1,26 @@
+package basic;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class BoardDTO {
+    private int bno;
+    private String title;
+    private String content;
+    private String writer;
+    private Date regdate;
+
+}
